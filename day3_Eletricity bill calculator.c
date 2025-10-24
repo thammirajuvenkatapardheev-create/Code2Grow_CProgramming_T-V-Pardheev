d1dd11ceb937b30a35c647c9b2a84e406115a21b
thammirajuvenkatapardheev-create <thammirajuvenkatapardheev@gmail.com>
2025-10-24 16:19:56 +0530

    Update day3_Eletricity bill calculator.c

--- day3_Eletricity bill calculator.c
+++ day3_Eletricity bill calculator.c
@@ -1,32 +1,24 @@
 #include<stdio.h>
 int main()
 {
-   int totalbill,units_consumed;
-   printf("enter total units consumed:");
-   scanf("%d",&units_consumed);
-   if(units_consumed<=100)
-   {
-       printf("the rate per unit is 3.00\n");
-       totalbill=units_consumed*3+50.0;
-       printf("the total bill is %d\n",totalbill);
-   }
-else if(units_consumed>=101 && units_consumed<=200)
-{
-    printf("the rate per unit is 4.50\n");
-    totalbill=units_consumed*4.50+50.0;
-    printf("the total bill is %d\n",totalbill);
-}
-else if(units_consumed>=201 && units_consumed<=300)
-{
-    printf("the rate per unit is 6.00\n");
-    totalbill=units_consumed*6.00+50.0;
-    printf("the total bill is %d\n",totalbill);
-}
-else
-{
-    printf("the rate per unit is 8.00\n");
-    totalbill=units_consumed*8.00+50.0;
-    printf("the total bill is %d\n",totalbill);
-}  
-return 0;
+    float units_consumed,total_bill;
+    printf("enter a number::");
+    scanf("%f",&units_consumed);
+
+    if(units_consumed<=100)
+    {
+        total_bill=units_consumed*3.00;
+    }else if(units_consumed>=101 && units_consumed<=200)
+    {
+        total_bill=units_consumed*4.50;
+    }else if (units_consumed>=201 && units_consumed<=300)
+    {
+        total_bill=units_consumed*6.00;
+    }else{
+        total_bill=units_consumed*8.00;
+    }
+    total_bill+=50;
+    printf("------Electricity bill -------\n");
+    printf("Total Cost for the %f is:::%.2f ",units_consumed,total_bill);
+    return 0;
 }
